0d4083df9eabd037b0373544081ba4a68afca54d
Lukas Masuch <lukas.masuch@gmail.com>
2019-09-20 18:06:08 +0200

    Adapt setup_certs from workspace script

--- resources/scripts/setup_certs.sh
+++ resources/scripts/setup_certs.sh
@@ -1,16 +1,20 @@
 SSLNAME=cert
 
-[ -f /run/secrets/$SSLNAME.crt ] && cp /run/secrets/$SSLNAME.crt ${_SSL_RESOURCES_PATH}/$SSLNAME.crt > /dev/null
-[ -f /run/secrets/$SSLNAME.key ] && cp /run/secrets/$SSLNAME.key ${_SSL_RESOURCES_PATH}/$SSLNAME.key > /dev/null
-[ -f /run/secrets/$SSLNAME.pem ] && cp /run/secrets/$SSLNAME.pem ${_SSL_RESOURCES_PATH}/$SSLNAME.pem > /dev/null
+[ -f /run/secrets/$SSLNAME.crt ] && cp /run/secrets/$SSLNAME.crt ${_SSL_RESOURCES_PATH}/$SSLNAME.crt
+[ -f /run/secrets/$SSLNAME.key ] && cp /run/secrets/$SSLNAME.key ${_SSL_RESOURCES_PATH}/$SSLNAME.key
+[ -f /run/secrets/$SSLNAME.pem ] && cp /run/secrets/$SSLNAME.pem ${_SSL_RESOURCES_PATH}/$SSLNAME.pem
 
 if [ ! -f ${_SSL_RESOURCES_PATH}/$SSLNAME.crt ]; then
+    echo "No certificate was provided for SSL/HTTPS."
+    echo "Generate self-signed certificate for SSL/HTTPS."
+
     SSLDAYS=365
-    echo "Generate self-signed SSL certificate since no were provided during startup."
-    openssl req -x509 -nodes -newkey rsa:2048 -keyout $SSLNAME.key -out $SSLNAME.crt  -days $SSLDAYS -subj '/C=DE/ST=Berlin/L=Berlin/CN=localhost' 2>/dev/null
+    openssl req -x509 -nodes -newkey rsa:2048 -keyout $SSLNAME.key -out $SSLNAME.crt  -days $SSLDAYS -subj '/C=DE/ST=Berlin/L=Berlin/CN=localhost' > /dev/null 2>&1
     
-    mv $SSLNAME.crt ${_SSL_RESOURCES_PATH}/$SSLNAME.crt > /dev/null 2>&1
-    mv $SSLNAME.key ${_SSL_RESOURCES_PATH}/$SSLNAME.key > /dev/null 2>&1
+    mv $SSLNAME.crt ${_SSL_RESOURCES_PATH}/$SSLNAME.crt
+    mv $SSLNAME.key ${_SSL_RESOURCES_PATH}/$SSLNAME.key
+else
+    echo "Certificate for SSL/HTTPS was found in "${SSL_RESOURCES_PATH}
 fi
 
 # trust certificate. used in case containers share the same certificate
