cae5c0f44b92957f9bd7392a236bdf0f7da71cdc
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2019-10-16 17:47:22 +0200

    Add subjectAltName to self-generated certificate as it should be set

--- resources/scripts/setup_certs.sh
+++ resources/scripts/setup_certs.sh
@@ -9,7 +9,8 @@ if [ ! -f ${_SSL_RESOURCES_PATH}/$SSLNAME.crt ]; then
     echo "Generate self-signed certificate for SSL/HTTPS."
 
     SSLDAYS=365
-    openssl req -x509 -nodes -newkey rsa:2048 -keyout $SSLNAME.key -out $SSLNAME.crt  -days $SSLDAYS -subj '/C=DE/ST=Berlin/L=Berlin/CN=localhost' > /dev/null 2>&1
+    touch /root/.rnd
+    openssl req -x509 -nodes -newkey rsa:2048 -keyout $SSLNAME.key -out $SSLNAME.crt  -days $SSLDAYS -subj '/C=DE/ST=Berlin/L=Berlin/CN=localhost' -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:localhost,DNS:127.0.0.1\n")) > /dev/null 2>&1
     
     mv $SSLNAME.crt ${_SSL_RESOURCES_PATH}/$SSLNAME.crt
     mv $SSLNAME.key ${_SSL_RESOURCES_PATH}/$SSLNAME.key
