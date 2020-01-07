c32f4ce7df386e40de773b787b771326de0df2de
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-01-07 17:12:45 +0100

    Fix way how GPUs are counted. Previously, the command `nvidia-smi` was not installed and even if it were, it would not have shown gpus as they would have needed to be made visible to mlhub

--- resources/mlhubspawner/mlhubspawner/mlhubspawner.py
+++ resources/mlhubspawner/mlhubspawner/mlhubspawner.py
@@ -340,22 +340,11 @@ class MLHubDockerSpawner(DockerSpawner):
     def get_gpu_info(self) -> list:
         count_gpu = 0
         try:
-            sp = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out_str = sp.communicate()
-            out_list = out_str[0].decode("utf-8").split('\n')
-
-            # out_dict = {}
-
-            for item in out_list:
-                try:
-                    key, val = item.split(':')
-                    key, val = key.strip(), val.strip()
-                    if key == 'Product Name':
-                        count_gpu += 1
-                        # gpus.append(val)
-                    #out_dict[key + "_" + str(count_gpu)] = val
-                except:
-                    pass
+            # NOTE: this approach currently only works for nvidia gpus.
+            ps = subprocess.Popen(('find', '/proc/irq/', '-name', 'nvidia'), stdout=subprocess.PIPE)
+            output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+            ps.wait()
+            count_gpu = int(output.decode("utf-8"))
         except:
             pass
 
